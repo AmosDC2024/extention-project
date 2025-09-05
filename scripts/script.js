93b5e9ad5ee2ca5bd68b1f3525465ccedd56247f
IMHOEDE AMOS OSIKEMHEKHA <osiamijnr@gmai.com>
2025-09-05 08:19:29 +0100

    fix toggle bug

--- scripts/script.js
+++ scripts/script.js
@@ -7,41 +7,67 @@ const card = document.querySelectorAll(".card")
 const head = document.querySelectorAll("h2") 
 const ctaRemove = document.querySelectorAll(".cta-remove")   
 const extentionList = document.querySelector("h1");
-console.log(ctaRemove);
-
 
 ctaActive.addEventListener("click",()=>{
   checkbox.forEach((item)=>{
     item.checked = !item.checked
   })
-}); 
+});  
 
-function mode(){
-  const lightMode = "/assets/images/icon-moon.svg"; 
-  modeToggle.src = lightMode;
-  document.body.style.backgroundImage = "linear-gradient(180deg, #EBF2FC 0%, #EEF8F9 100%)";
-  extention.style.backgroundColor = "hsl(217, 61%, 90%)";   
+ let light = true; 
+
+function lightMode(){ 
+  if (!light){ 
+    const lightMode = "/assets/images/icon-moon.svg";   
+    modeToggle.src = lightMode;
+    document.body.style.backgroundImage = "linear-gradient(180deg, #EBF2FC 0%, #EEF8F9 100%)";
+    extention.style.backgroundColor = "hsl(217, 61%, 90%)";   
+
+    card.forEach((item)=>{ 
+      item.style.backgroundColor ="hsl(200, 60%, 99%)"
+    }) 
+
+      head.forEach((item) => {
+        item.classList.remove("dark-mode"); 
+         item.classList.add("light-mode");    
+      });
+
+      ctaRemove.forEach((item) => {
+        item.classList.remove("cta-dark-mode", light);
+        item.classList.toggle("cta-light-mode", !light);
+      });
+
+    extentionList.style.color = "hsl(226, 25%, 17%)"; 
+
+    light = true
+  } else{ 
+      const darkMode = "/assets/images/icon-sun.svg";
+      modeToggle.src = darkMode;
+      document.body.style.backgroundImage = "linear-gradient(180deg, #040918 0%, #091540 100%)";
+      extention.style.backgroundColor = "hsl(226, 25%, 17%)";   
+
+      card.forEach((item)=>{ 
+        item.style.backgroundColor ="hsl(226, 25%, 17%)"
+      }) 
+
+      head.forEach((item) => {
+        item.classList.remove("light-mode");
+        item.classList.add("dark-mode");
+      });
+
+      ctaRemove.forEach((item) => {
+        item.classList.remove("cta-light-mode");
+        item.classList.add("cta-dark-mode");
+      });
 
 
-  // head.style.backgroundColor = "hsl(226, 25%, 17%)";
-  card.forEach((item)=>{ 
-    item.style.backgroundColor ="hsl(0, 0%, 93%)"
-  }) 
+      extentionList.style.color = "hsl(200, 60%, 99%)";
 
-  head.forEach((item)=>{
-    item.classList.add("light-mode")
-  }); 
-
-  
-  ctaRemove.forEach((item)=>{
-    item.classList.add("cta-light-mode")
-  }); 
-
-  extentionList.style.color = "hsl(226, 25%, 17%)";
-
-  // head.classList.add("light-mode");
-} ;
+      light = false;
+    } ;
+  };
  
-modeToggle.addEventListener("click", mode);
+ 
+modeToggle.addEventListener("click", lightMode); 
 
 
